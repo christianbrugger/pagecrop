f58a44ecef74eda1a557d155c37104da871a08b1
Christian Brugger <brugger.chr@gmail.com>
2020-02-06 21:01:06 +0100

    added canny based crop && jpeg2000 export

--- crop.py
+++ crop.py
@@ -1,5 +1,11 @@
 
+"""
+pip install glymur==0.8.19
+pip install img2pdf
+"""
+
 import math
+import os
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +13,8 @@ try:
     import cv2.cv2 as cv2
 except ImportError:
     import cv2
+import glymur
+import img2pdf
 
 from utils import show_images, round_up_to_odd, time_ctx, show_histogram
 
@@ -14,6 +22,9 @@ from utils import show_images, round_up_to_odd, time_ctx, show_histogram
 def biggest_contour(contours):
     areas = list(map(cv2.contourArea, contours))
     return contours[np.argmax(areas)]
+
+def resize(img, factor):
+    return cv2.resize(img, (0,0), fx=factor, fy=factor, interpolation=cv2.INTER_CUBIC)
 
 
 def get_contour_color(img, border=0.05, _debug=True):
@@ -37,22 +48,28 @@ def get_contour_color(img, border=0.05, _debug=True):
     return contour
 
 
-def get_contour_canny(img, border=0.05, _debug=True):
-    # blurr
-    blurr = cv2.medianBlur(img, 50)
+def convex_area(contour):
+    hull = cv2.convexHull(contour)
+    return cv2.contourArea(hull)
 
-    # mask border color
-    hsv = cv2.cvtColor(blurr, cv2.COLOR_BGR2HSV)
-    patch = hsv[:math.ceil(hsv.shape[0] * border), :math.ceil(hsv.shape[1] * border)]
-    bg_hue = np.median(patch[:,:,0])
-    mask = cv2.inRange(hsv, np.array([bg_hue - 10, 100, 20]), np.array([bg_hue + 10, 255, 255]))
 
-    # remove holes
-    contours = cv2.findContours(cv2.bitwise_not(mask), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-    contour = biggest_contour(contours)
+def get_contour_canny(img, ksize_smooth=7, _debug=False):
+    scale = 500 / img.shape[0]
+    resized = resize(img, scale)
+
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    blurr = cv2.medianBlur(gray, ksize_smooth)
+    opened = cv2.morphologyEx(blurr, cv2.MORPH_OPEN, np.ones((ksize_smooth,)*2, np.uint8))
+    edged = cv2.Canny(opened, 40, 200)
+    closed_edged = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, np.ones((ksize_smooth,)*2, np.uint8))
+
+    contours = cv2.findContours(closed_edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_KCOS)[0]
+    #hulls = map(cv2.convexHull, contours)
+    raw_contour = max(contours, key=convex_area)
+    contour = np.round(raw_contour.reshape(-1, 2) / scale).astype(int)
 
     if _debug:
-        show_images([blurr, mask, with_contour(img, contour)])
+        show_images([opened, edged, closed_edged, with_contour(img, contour)])
 
     return contour
 
@@ -101,7 +118,7 @@ def crop_page(img, contour):
     return crop
 
 
-def equalize_histogram(img, threshold=0.05, min_white=150, _debug=False):
+def correct_brightness(img, threshold=0.05, min_white=150, _debug=False):
     yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
 
     # white point
@@ -114,7 +131,7 @@ def equalize_histogram(img, threshold=0.05, min_white=150, _debug=False):
     # adjust intensity
     gray = yuv[:,:,0].astype(float) / 255.
     gray_white = gray * 255 / white
-    gray_gamma = gray_white ** (255 / white)
+    gray_gamma = gray_white ** ((255 / white) ** 0.75)
 
     yuv[:,:,0] = np.clip(gray_gamma * 255, 0, 255).astype(np.uint8)
     output = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
@@ -128,26 +145,41 @@ def equalize_histogram(img, threshold=0.05, min_white=150, _debug=False):
 
 
 def extract_page(img):
-    with time_ctx("get_contour"):
-        contour = get_contour(img)
-
-    with time_ctx("crop_page"):
-        page = crop_page(img, contour)
-
-    page = equalize_histogram(page)
-
+    contour = get_contour_canny(img)
+    page = crop_page(img, contour)
+    page = correct_brightness(page)
     return page
 
 
 def main():
-    with time_ctx("read image"):
-        test = cv2.imread("test_2.JPG")
+    names_jp2 = []
+    names_jpg = []
+    for i, name in enumerate(os.listdir(".")):
+        if name.startswith("test_"):
 
-    page = extract_page(test)
+            with time_ctx("read image"):
+                test = cv2.imread(name)
 
-    cv2.imwrite("output.png", page)
+            with time_ctx("extract page"):
+                page = extract_page(test)
 
-    show_images([page])
+            with time_ctx("store jpg"):
+                outname = 'output_{}.jpg'.format(i)
+                cv2.imwrite(outname, page, (cv2.IMWRITE_JPEG_QUALITY, 60))
+                names_jpg.append(outname)
+
+            with time_ctx("store jpeg2000"):
+                outname = 'output_{}.jp2'.format(i)
+                glymur.Jp2k(outname, page[:,:,::-1], irreversible=True, cratios=[50])
+                names_jp2.append(outname)
+
+    with open("output_jpg.pdf", "wb") as f:
+        f.write(img2pdf.convert(names_jpg))
+    with open("output_jp2.pdf", "wb") as f:
+        f.write(img2pdf.convert(names_jp2))
+
+
+        #show_images([page])
 
 
 if __name__ == "__main__":
